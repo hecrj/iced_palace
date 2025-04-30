e6b51ea1e0bb6fa09b45ad1a97f978f952b96906
Héctor Ramón Jiménez <hector@hecrj.dev>
2025-04-30 05:17:35 +0200

    Feature-gate `rand` dependency and `diffused_text`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod widget;
 
+#[cfg(any(feature = "rand", feature = "geometry"))]
 use iced_core as core;
--- src/widget.rs
+++ src/widget.rs
@@ -1,33 +1,33 @@
+#[cfg(feature = "rand")]
 mod diffused_text;
 
 #[cfg(feature = "geometry")]
 mod dynamic_text;
 
+#[cfg(feature = "rand")]
 pub use diffused_text::DiffusedText;
 
 #[cfg(feature = "geometry")]
 pub use dynamic_text::DynamicText;
 
-use crate::core::text;
-use crate::core::widget;
-
+#[cfg(feature = "rand")]
 pub fn diffused_text<'a, Theme, Renderer>(
-    fragment: impl text::IntoFragment<'a>,
+    fragment: impl crate::core::text::IntoFragment<'a>,
 ) -> DiffusedText<'a, Theme, Renderer>
 where
-    Theme: widget::text::Catalog,
-    Renderer: text::Renderer,
+    Theme: crate::core::widget::text::Catalog,
+    Renderer: crate::core::text::Renderer,
 {
     DiffusedText::new(fragment)
 }
 
 #[cfg(feature = "geometry")]
 pub fn dynamic_text<'a, Theme, Renderer>(
-    fragment: impl text::IntoFragment<'a>,
+    fragment: impl crate::core::text::IntoFragment<'a>,
 ) -> DynamicText<'a, Theme, Renderer>
 where
-    Theme: widget::text::Catalog,
-    Renderer: text::Renderer,
+    Theme: crate::core::widget::text::Catalog,
+    Renderer: crate::core::text::Renderer,
 {
     DynamicText::new(fragment)
 }
