8d6ab17090b30911ae48b665df74addea6bc16bb
Héctor Ramón Jiménez <hector@hecrj.dev>
2025-12-18 17:19:22 +0100

    Disable hinting for `dynamic_text` widget

--- src/widget/dynamic_text.rs
+++ src/widget/dynamic_text.rs
@@ -185,7 +185,7 @@ where
                 align_y: self.align_y,
                 shaping: self.shaping,
                 wrapping: text::Wrapping::default(),
-                hint_factor: renderer.scale_factor(),
+                hint_factor: None,
             });
 
             if changed {
